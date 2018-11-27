6f8cb2e9f02491c33b39e777c1f9d0e0dd1c5871
imflop <imflop@gmail.com>
2018-11-27 16:25:17 +0300

    init2

--- api.py
+++ api.py
@@ -10,102 +10,104 @@ api = Api(app)
 db = redis.StrictRedis(host='localhost', port=6379, db=1)
 
 
-@api.route('/api/v1/status/<branch>')
-class Status(Resource):
-    def get(self, branch):
-        """
-        Api status (not now)
-        :return: Dummy status
-        """
-        if branch:
-            if db.exists(branch):
-                data = db.get(branch)
-                return dict(branch=branch, meta=json.loads(data), code=200, status='OK')
-        else:
-            return dict(status='Not Found', code=404)
-
-
 @api.route('/api/v1/create')
 class Create(Resource):
     def post(self) -> dict:
         branch = api.payload.get('branch')
         ip = api.payload.get('ip')
-        if self._is_ip_exists(ip):
-            if self._is_branch_exists(branch):
-                project_data = self._get_project_meta_by_branch_name(branch)
+        if Utils.is_ip_exists(ip):
+            if Utils.is_branch_exists(branch):
+                project_data = Utils.get_project_meta_by_branch(branch)
                 port = project_data['port']
-                return self.response_dict(code=304, status='Not Modified', ip=ip, port=port,
-                                          message='Branch already exists on current port')
+                return Utils.response_dict(code=304, status='Not Modified', ip=ip, port=port,
+                                           message='Branch already exists on current port')
             else:
-                port = self._get_first_free_port_by_ip(ip)
+                port = Utils.get_first_free_port_by_ip(ip)
                 project_data = dict(
                     created_at=datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
                     ip=ip,
                     port=port
                 )
-                self._set_project_meta_to_branch(branch, project_data)
-                return self.response_dict(code=201, status='Created', ip=ip, port=port, message='OK')
+                Utils.set_project_meta_to_branch(branch, project_data)
+                return Utils.response_dict(code=201, status='Created', ip=ip, port=port, message='OK')
         else:
-            self._set_ports_to_ip(ip)
-            port = self._get_first_free_port_by_ip(ip)
+            Utils.set_ports_to_ip(ip)
+            port = Utils.get_first_free_port_by_ip(ip)
             project_data = dict(
                 created_at=datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
                 ip=ip,
                 port=port
             )
-            self._set_project_meta_to_branch(branch, project_data)
-            return self.response_dict(code=201, status='Created', ip=ip, port=port, message='OK')
+            Utils.set_project_meta_to_branch(branch, project_data)
+            return Utils.response_dict(code=201, status='Created', ip=ip, port=port, message='OK')
 
-    def _is_ip_exists(self, ip: str) -> bool:
+
+@api.route('/api/v1/delete')
+class Delete(Resource):
+    def delete(self):
+        """
+        Delete meta data from redis, also change port status
+        :return: 204 if exists else 404
+        """
+        ip = api.payload.get('ip')
+        branch = api.payload.get('branch')
+        if Utils.is_branch_exists(branch) and Utils.is_ip_exists(ip):
+            data = Utils.get_project_meta_by_branch(branch)
+            port = data['port']
+            ports = Utils.get_ports_by_ip(ip)
+            ports[port] = True
+            Utils.set_release_port(ip, ports)
+            db.delete(branch)
+            return Utils.response_dict(code=202, status='Accepted', ip=ip, port=port,
+                                       message='Branch remove, current port release')
+        else:
+            return Utils.response_dict(status='Not Found', code=404)
+
+
+class Utils:
+    @staticmethod
+    def response_dict(code: int, status: str, ip=None, port=None, message=None) -> dict:
+        if not ip and not port and not message:
+            return dict(code=code, status=status)
+        else:
+            return dict(code=code, status=status, ip=ip, port=port, message=message)
+
+    @staticmethod
+    def is_ip_exists(ip: str) -> bool:
         return True if db.exists(ip) else False
 
-    def _is_branch_exists(self, branch: str) -> bool:
+    @staticmethod
+    def is_branch_exists(branch: str) -> bool:
         return True if db.exists(branch) else False
 
-    def _set_ports_to_ip(self, ip: str) -> bool:
+    @staticmethod
+    def set_ports_to_ip(ip: str) -> None:
         ports_dict = {_: True for _ in range(8100, 8300)}
-        return db.set(ip, json.dumps(ports_dict))
+        db.set(ip, json.dumps(ports_dict))
 
-    def _set_project_meta_to_branch(self, branch: str, data: dict) -> None:
+    @staticmethod
+    def set_release_port(ip: str, ports: dict) -> None:
+        db.set(ip, json.dumps(ports))
+
+    @staticmethod
+    def set_project_meta_to_branch(branch: str, data: dict) -> None:
         db.set(branch, json.dumps(data))
 
-    def _get_project_meta_by_branch_name(self, branch: str) -> dict:
-        d = db.get(branch)
-        return json.loads(d)
+    @staticmethod
+    def get_project_meta_by_branch(branch: str) -> dict:
+        return json.loads(db.get(branch))
 
-    def _get_project_data_by_ip(self, ip: str) -> dict:
-        d = db.get(ip)
-        return json.loads(d)
+    @staticmethod
+    def get_ports_by_ip(ip: str) -> dict:
+        return json.loads(db.get(ip))
 
-    def _get_first_free_port_by_ip(self, ip: str) -> int:
+    @staticmethod
+    def get_first_free_port_by_ip(ip: str) -> int:
         ports = json.loads(db.get(ip))
         key = [k for k, v in ports.items() if v is True][0]
         ports[key] = False
         db.set(ip, json.dumps(ports))
         return int(key)
-
-    def response_dict(self, code: int, status: str, ip: str, port: int, message: str) -> dict:
-        return dict(code=code, status=status, ip=ip, port=port, message=message)
-
-
-@api.route('/api/v1/delete/<project_name>')
-class Delete(Resource):
-    def delete(self, project_name):
-        """
-        Delete project from redis
-        :param project_name: string name of project
-        :return: 204 if exists else 404
-        """
-        if db.exists(project_name):
-            db.delete(project_name)
-            return dict(status='Accepted', code=202)
-        else:
-            return dict(status='Not Found', code=404)
-
-
-def _get_rnd_port() -> int:
-    from random import randint
-    return randint(1025, 65535)
 
 
 if __name__ == '__main__':
