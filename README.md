6300db07c2d8065471e9469ff9370fa860b1c018
imflop <imflop@gmail.com>
2018-11-28 14:39:44 +0300

    Рефакторинг
    - вынес работу с бд в миксин
    - добавил объект ответа
    - добавил ридми

---
+++ README.md
@@ -0,0 +1,37 @@
+# Начало работы:
+
+### 1 Создать запись в бд - получить ответ
+
+
+Request POST
+
+`http post url:5000/api/v1/create branch=feature/branch-name ip=127.0.0.1`
+
+Response
+
+```json
+{
+    "code": 201,
+    "status": "Created",
+    "ip": "127.0.0.1",
+    "port": 8100,
+    "message": "Some text message"
+}
+```
+
+
+### 2 Удалить запись, освободить порт - получить ответ
+
+Request DELETE
+
+`http delete localhost:5000/api/v1/delete branch=feature/branch-name, ip=127.0.0.1`
+
+Response
+
+```json
+{
+    "code": "202",
+    "status": "Accepted",
+    "message": "Branch remove, current port release"
+}
+```
--- api.py
+++ api.py
@@ -10,40 +10,78 @@ api = Api(app)
 db = redis.StrictRedis(host='localhost', port=6379, db=1)
 
 
+class DbMixin:
+
+    def is_ip_exists(self, ip: str) -> bool:
+        return True if db.exists(ip) else False
+
+    def is_branch_exists(self, branch: str) -> bool:
+        return True if db.exists(branch) else False
+
+    def set_ports_to_ip(self, ip: str) -> None:
+        ports_dict = {_: True for _ in range(8100, 8300)}
+        db.set(ip, json.dumps(ports_dict))
+
+    def set_released_port(self, ip: str, ports: dict) -> None:
+        db.set(ip, json.dumps(ports))
+
+    def set_project_meta_to_branch(self, branch: str, data: dict) -> None:
+        db.set(branch, json.dumps(data))
+
+    def get_project_meta_by_branch(self, branch: str) -> dict:
+        return json.loads(db.get(branch))
+
+    def get_ports_by_ip(self, ip: str) -> dict:
+        return json.dumps(db.get(ip))
+
+    def get_first_free_port_by_ip(self, ip: str) -> int:
+        ports = json.loads(db.get(ip))
+        key = [k for k, v in ports.items() if v is True][0]
+        ports[key] = False
+        db.set(ip, json.dumps(ports))
+        return int(key)
+
+    def delete_branch(self, branch: str) -> None:
+        db.delete(branch)
+
+
 @api.route('/api/v1/create')
-class Create(Resource):
+class Create(Resource, DbMixin):
     def post(self) -> dict:
         branch = api.payload.get('branch')
         ip = api.payload.get('ip')
-        if Utils.is_ip_exists(ip):
-            if Utils.is_branch_exists(branch):
-                project_data = Utils.get_project_meta_by_branch(branch)
+        if self.is_ip_exists(ip):
+            if self.is_branch_exists(branch):
+                project_data = self.get_project_meta_by_branch(branch)
                 port = project_data['port']
-                return Utils.response_dict(code=304, status='Not Modified', ip=ip, port=port,
-                                           message='Branch already exists on current port')
+                response = ResponseObject(code=304, status='Not Modified', ip=ip, port=port,
+                                          message='Branch already exists on current port')
             else:
-                port = Utils.get_first_free_port_by_ip(ip)
+                port = self.get_first_free_port_by_ip(ip)
                 project_data = dict(
                     created_at=datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
                     ip=ip,
                     port=port
                 )
-                Utils.set_project_meta_to_branch(branch, project_data)
-                return Utils.response_dict(code=201, status='Created', ip=ip, port=port, message='OK')
+                self.set_project_meta_to_branch(branch, project_data)
+                response = ResponseObject(code=201, status='Created', ip=ip, port=port,
+                                          message='New record successfully created')
         else:
-            Utils.set_ports_to_ip(ip)
-            port = Utils.get_first_free_port_by_ip(ip)
+            self.set_ports_to_ip(ip)
+            port = self.get_first_free_port_by_ip(ip)
             project_data = dict(
                 created_at=datetime.now().strftime("%d-%m-%Y %H:%M:%S"),
                 ip=ip,
                 port=port
             )
-            Utils.set_project_meta_to_branch(branch, project_data)
-            return Utils.response_dict(code=201, status='Created', ip=ip, port=port, message='OK')
+            self.set_project_meta_to_branch(branch, project_data)
+            response = ResponseObject(code=201, status='Created', ip=ip, port=port,
+                                      message='New record successfully created')
+        return response.as_dict()
 
 
 @api.route('/api/v1/delete')
-class Delete(Resource):
+class Delete(Resource, DbMixin):
     def delete(self):
         """
         Delete meta data from redis, also change port status
@@ -51,63 +89,30 @@ class Delete(Resource):
         """
         ip = api.payload.get('ip')
         branch = api.payload.get('branch')
-        if Utils.is_branch_exists(branch) and Utils.is_ip_exists(ip):
-            data = Utils.get_project_meta_by_branch(branch)
+        if self.is_branch_exists(branch) and self.is_ip_exists(ip):
+            data = self.get_project_meta_by_branch(branch)
             port = data['port']
-            ports = Utils.get_ports_by_ip(ip)
+            ports = self.get_ports_by_ip(ip)
             ports[port] = True
-            Utils.set_release_port(ip, ports)
-            db.delete(branch)
-            return Utils.response_dict(code=202, status='Accepted', ip=ip, port=port,
-                                       message='Branch remove, current port release')
+            self.set_released_port(ip, ports)
+            self.delete_branch(branch)
+            response = ResponseObject(code=202, status='Accepted', message='Branch remove, current port release')
         else:
-            return Utils.response_dict(status='Not Found', code=404)
+            response = ResponseObject(status='Not Found', code=404)
+        return response.as_dict()
 
 
-class Utils:
-    @staticmethod
-    def response_dict(code: int, status: str, ip=None, port=None, message=None) -> dict:
-        if not ip and not port and not message:
-            return dict(code=code, status=status)
-        else:
-            return dict(code=code, status=status, ip=ip, port=port, message=message)
+class ResponseObject:
 
-    @staticmethod
-    def is_ip_exists(ip: str) -> bool:
-        return True if db.exists(ip) else False
+    def __init__(self, code: int, status: str, ip: str = None, port: int = None, message: str = None):
+        self.code = code
+        self.status = status
+        self.ip = ip
+        self.port = port
+        self.message = message
 
-    @staticmethod
-    def is_branch_exists(branch: str) -> bool:
-        return True if db.exists(branch) else False
-
-    @staticmethod
-    def set_ports_to_ip(ip: str) -> None:
-        ports_dict = {_: True for _ in range(8100, 8300)}
-        db.set(ip, json.dumps(ports_dict))
-
-    @staticmethod
-    def set_release_port(ip: str, ports: dict) -> None:
-        db.set(ip, json.dumps(ports))
-
-    @staticmethod
-    def set_project_meta_to_branch(branch: str, data: dict) -> None:
-        db.set(branch, json.dumps(data))
-
-    @staticmethod
-    def get_project_meta_by_branch(branch: str) -> dict:
-        return json.loads(db.get(branch))
-
-    @staticmethod
-    def get_ports_by_ip(ip: str) -> dict:
-        return json.loads(db.get(ip))
-
-    @staticmethod
-    def get_first_free_port_by_ip(ip: str) -> int:
-        ports = json.loads(db.get(ip))
-        key = [k for k, v in ports.items() if v is True][0]
-        ports[key] = False
-        db.set(ip, json.dumps(ports))
-        return int(key)
+    def as_dict(self):
+        return {k: v for k, v in filter(lambda x: x[1] is not None, vars(self).items())}
 
 
 if __name__ == '__main__':
